e95b7327ab24b7e83deb251922aa38fc85c38ad5
Ricardo Rosas <ricardo.rosasv@gmail.com>
2022-01-21 23:54:09 +0100

    static, media y gitignore

--- tpm_webapp/settings.py
+++ tpm_webapp/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Third party
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -149,6 +151,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+from .cdn.conf import * #noqa
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
