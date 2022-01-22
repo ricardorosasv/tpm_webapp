4eb9b7d8955d6bc77b5fa7e5f8385799410fee0b
Ricardo Rosas <ricardo.rosasv@gmail.com>
2022-01-22 15:16:26 +0100

    Added whitenoise

--- tpm_webapp/settings.py
+++ tpm_webapp/settings.py
@@ -32,8 +32,6 @@ if not DEBUG:
 # Application definition
 
 INSTALLED_APPS = [
-    'main_app',
-    'mantt',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #Third party
     'storages',
+    #Own
+    'main_app',
+    'mantt',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'tpm_webapp.urls'
@@ -150,9 +152,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 from .cdn.conf import * #noqa
 
+# Extra places for collectstatic to find static files.
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
